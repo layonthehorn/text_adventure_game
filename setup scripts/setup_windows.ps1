a64e669bb4484273a56abcbf6c16af8f4c1e860c
layonthehorn <thomaspdressel@gmail.com>
2020-08-09 15:56:23 -0700

    updated PowerShell script and README.md

--- setup scripts/setup_windows.ps1
+++ setup scripts/setup_windows.ps1
@@ -2,7 +2,9 @@ $ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
 cd $ScriptDir
 cd ..
 python -m venv env
-.env\Scripts\activate.ps1
+env\Scripts\activate.ps1
 pip install --upgrade pip
 pip install colorama
 pause
+
+python .\Vern_Adventures.py
