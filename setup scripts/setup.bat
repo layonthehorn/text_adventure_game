ba6cc02ac69d0653fec2fd8c81795c72539565f0
layonthehorn <thomaspdressel@gmail.com>
2020-08-09 14:44:20 -0700

    Changed path handling in bat file.

--- setup scripts/setup.bat
+++ setup scripts/setup.bat
@@ -1,11 +1,11 @@
 echo "Setting up VENV."
-python3 -m venv %~dp0\..\env
+python3 -m venv %~dp0..\env
 
 echo "Updating pip"
-%~dp0\..\env\bin\pip install --upgrade pip
+%~dp0..\env\bin\pip install --upgrade pip
 
 echo "Installing Packages to VENV"
-%~dp0\..\env\bin\pip install xdg colorama
+%~dp0..\env\bin\pip install xdg colorama
 
 echo "Setup complete"
 pause
