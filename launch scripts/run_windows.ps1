75c7cf57a8e6d6e6723ab5541fdf0fdf93a53b62
layonthehorn <thomaspdressel@gmail.com>
2020-08-09 17:13:49 -0700

    fixed errors in windows launch script.

--- launch scripts/run_windows.ps1
+++ launch scripts/run_windows.ps1
@@ -1,24 +1,24 @@
 $ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
-cd $ScriptDir
-cd ..
-if (not Test-Path -Path "env\")
+Set-Location $ScriptDir
+Set-Location ..
+if ( Test-Path -Path "env\")
 {
-    echo "This script requires you to have python3.7 or later installed and on your path."
-    echo "This will also launch the game every time so you may continue to use it to play later."
-    echo ""
+    env\Scripts\activate.ps1
+    Write-Output "Launching game..."
+
+}
+else
+{
+    Write-Output "This script requires you to have python3.7 or later installed and on your path."
+    Write-Output "This will also launch the game every time so you may continue to use it to play later."
+    Write-Output ""
     pause
     python -m venv env
     env\Scripts\activate.ps1
     pip install --upgrade pip
     pip install colorama
-    echo "Launching game..."
-    pause
-}
-else
-{
-    env\Scripts\activate.ps1
-    echo "Launching game..."
-    pause
-}
+    Write-Output "Launching game..."
 
+}
+pause
 python .\Vern_Adventures.py
